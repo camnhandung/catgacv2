eaf0539929200866819548156ce001099da1ba74
camnhandung <camnhandungk88@gmail.com>
2026-05-06 22:57:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ from models import (
     ShiftAssignment,
     IncidentReport
 )
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app = FastAPI(title="Hệ thống Điều hành Cắt gác CQTM", version="3.1")
 
+app = FastAPI(title="Hệ thống Điều hành Cắt gác CQTM", version="3.1")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(
     CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
